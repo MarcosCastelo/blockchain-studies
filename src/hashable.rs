364410b2bd44d2d6304039ef67f24c62fd9b9e66
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-01-26 00:41:50 -0300

    Fixing some troubles

--- src/hashable.rs
+++ src/hashable.rs
@@ -2,5 +2,6 @@ pub trait Hashable {
     fn bytes (&self) -> Vec<u8>;
 
     fn hash(&self) -> Vec<u8> {
-       crypto_hash::digest(crypt_hash::Algorith::SHA256, &self.bytes()) 
+       crypto_hash::digest(crypto_hash::Algorithm::SHA256, &self.bytes()) 
     }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use blockchainlib::*;
 
 fn main() {
-    let block = Block::new(12, now(), vec![0; 32], 0, "Marcos block!".to_owned());
+    let mut block = Block::new(12, now(), vec![0; 32], 0, "Marcos block!".to_owned());
+
+    println!("{:?}", &block);
+
+    let h = block.hash();
+
+    println!("{:?}", &h);
+
+    block.hash = h;
 
     println!("{:?}", &block);
 }
