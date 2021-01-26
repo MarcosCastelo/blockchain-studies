87be4e8a71e30d49d77a35296bc7541318094b98
MarcosCastelo <marcosvitorcastelo@gmail.com>
2021-01-25 22:24:46 -0300

    Creating block file and adding formatter of block

---
+++ src/block.rs
@@ -0,0 +1,10 @@
+use std::fmt::{ self, Debug, Formatter };
+
+pub struct Block {
+}
+
+impl Debug for Block {
+    fn fmt (&self, f: &mut Formatter) -> fmt::Result {
+        write!(f, "Block")
+    }
+}
